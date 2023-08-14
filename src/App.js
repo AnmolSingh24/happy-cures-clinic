82fb048790beb4e18c8b2a362d84f0cd7e7ccb0e
Anmol <as0796348@gmail.com>
2023-08-14 20:29:39 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./components/pages/home";
+import About from "./components/pages/about";
+import Blogs from "./components/pages/blogs"
+import Doctor from "./components/pages/doctors"
+import Gallery from "./components/pages/gallery"
+import Appointment from "./components/pages/appointment"
+import Service from "./components/pages/service"
+import Contact from "./components/pages/contact"
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-    </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/doctors" element={<Doctor />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div >
   );
 }
 
