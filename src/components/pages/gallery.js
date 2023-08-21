88c405bb0c54c49f4081c5c101a1145650ecd0a6
Anmol <as0796348@gmail.com>
2023-08-21 18:47:25 +0530

    gallery page completed

--- src/components/pages/gallery.js
+++ src/components/pages/gallery.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import './gallery.css'
 
 const Gallery = () => {
+    const galleryImages = [
+        'https://res.cloudinary.com/djdblxcxt/image/upload/v1692623534/gallery-img4_j2a6co.jpg',
+        'https://res.cloudinary.com/djdblxcxt/image/upload/v1692623533/gallery-img1_hbo4po.jpg',
+        'https://res.cloudinary.com/djdblxcxt/image/upload/v1692623534/gallery-img2.jpg_ghtamb.jpg',
+        'https://res.cloudinary.com/djdblxcxt/image/upload/v1692623674/gallery-img6_k54yhl.jpg',
+        'https://res.cloudinary.com/djdblxcxt/image/upload/v1692623674/gallery-img5_tga5hm.webp'
+    ];
 
     return (
-        <div></div>
-    )
-}
+        <div className="img-container">
+            <div className="main-content">
+                <h1>Gallery</h1>
+                <div className="gallery">
+                    {galleryImages.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={`Image ${index}`}
+                            className="gallery-image"
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Gallery;
