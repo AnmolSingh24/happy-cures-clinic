90f1c54a21ba25e1ba65f47ae9c84c3719a8a844
Anmol <as0796348@gmail.com>
2023-08-21 19:52:17 +0530

    Appointment page completed

--- src/components/pages/appointment.js
+++ src/components/pages/appointment.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import './appointment.css'
 
 const Appointment = () => {
 
     return (
-        <div></div>
+        <div className='form'>
+            <h1>Book an Appointment</h1>
+            <form className='form-container'>
+                <input type='text' placeholder='Fullname' /><br />
+                <input type='email' placeholder='Email' /><br />
+                <input type='tel' placeholder='Phone Number' /><br />
+                <input type='text' placeholder='Date' /><br />
+                <div className='address-container'>
+                    <div className='address'>
+                        <input type='text' placeholder='Address' />
+                        <div>
+                            <input type='text' placeholder='City' />
+                        </div>
+                    </div>
+                    <div className='address'>
+                        <input type='text' placeholder='State' />
+                        <div>
+                            <input type='text' placeholder='Pincode' />
+                        </div>
+                    </div>
+                </div>
+                <button>Submit</button>
+            </form>
+        </div>
     )
 }
 
